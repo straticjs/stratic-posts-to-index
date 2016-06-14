8d41cbea268e715459de1b10349405a94de55300
Alex Jordan <alex@strugee.net>
2016-06-14 15:15:06 -0700

    Add most of the implementation

--- index.js
+++ index.js
@@ -16,11 +16,47 @@
 'use strict';
 
 var through2 = require('through2');
+var eos = require('end-of-stream');
+var vfs = require('vinyl-fs');
 
-module.exports = function(options) {
-	return through2.obj(function(file, enc, callback) {
-		var that = this;
+module.exports = function(template, options) {
+	if (typeof template !== 'string') throw new Error('template not specified');
+
+	// Open template stream early for read performance
+	var templateStream = vfs.src(template);
+
+	var files = [];
+	var that;
+	var callback;
+
+	var attachFiles = through2.obj(function(file, enc, _callback) {
+		file.data = file.data || {};
+		file.data.posts = files;
+
+		console.log('Pushing file:', file.inspect());
+
 		that.push(file);
+
+		_callback();
+	});
+
+	// For each file that comes through, add it to an array
+	var stream = through2.obj(function(file, enc, _callback) {
+		that = this;
+
+		files.push(file);
+
+		callback = _callback;
+	});
+
+	// Wait until all files have come through, then pipe to the attacher
+	eos(stream, function(err) {
+		if (err) throw err;
+
+		templateStream.pipe(attachFiles);
+
 		callback();
 	});
+
+	return stream;
 };
