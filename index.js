69e72bd6cae7c0472e05df572c48ecc2e702816b
Alex Jordan <alex@strugee.net>
2016-09-21 23:20:20 -0700

    Implement month and year indexes

--- index.js
+++ index.js
@@ -15,6 +15,8 @@
 
 'use strict';
 
+var path = require('path');
+if (!Array.prototype.includes) require('es7-array.prototype.includes');
 var through2 = require('through2');
 var eos = require('end-of-stream');
 var gutil = require('gulp-util');
@@ -41,9 +43,70 @@ module.exports = function(template, options) {
 		if (!templateFile) throw new Error('template not found in stream');
 
 		templateFile.data = templateFile.data || {};
-		templateFile.data.posts = files;
 
-		this.push(templateFile);
+		// Main index
+		var mainIndexFile = templateFile.clone();
+		mainIndexFile.data.posts = files;
+		this.push(mainIndexFile);
+
+		// Years
+		var years = [];
+		files.forEach(function(post) {
+			var year = new Date(post.time.epoch * 1000).getFullYear();
+			if (!years.includes(year)) years.push(year);
+		});
+
+		years.forEach(function(year) {
+			var file = templateFile.clone();
+
+			file.data.posts = files.filter(function(post) {
+				return year === new Date(post.time.epoch * 1000).getFullYear();
+			});
+
+			var yearStr = year.toString();
+
+			file.path = path.join(file.base, yearStr, file.relative);
+
+			this.push(file);
+		}, this);
+
+		// Months
+		// Map keys are years that posts have been authored in, value is array of months in that year
+		var months = new Map();
+		files.forEach(function(post) {
+			var date = new Date(post.time.epoch * 1000);
+			var year = date.getFullYear();
+			var month = date.getMonth();
+
+			if (!months.get(year)) months.set(year, []);
+
+			if (!months.get(year).includes(month)) months.get(year).push(month);
+		});
+
+		months.forEach(function(monthsInYear, year) {
+			monthsInYear.forEach(function(month) {
+				var file = templateFile.clone();
+
+				file.data.posts = files.filter(function(post) {
+					var date = new Date(post.time.epoch * 1000);
+					var postYear = date.getFullYear();
+					var postMonth = date.getMonth();
+
+					return year === postYear && month === postMonth;
+				});
+
+				var yearStr = year.toString();
+				var monthStr = month + 1;
+				monthStr = monthStr < 10 ? '0' + monthStr.toString() : monthStr.toString();
+
+				file.path = path.join(file.base, yearStr, monthStr, file.relative);
+
+				this.push(file);
+			}, this);
+		}, this);
+
+		// Categories
+		// TODO
 
 		callback();
 	});
